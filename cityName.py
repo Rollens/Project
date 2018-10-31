f2029d5da7ea3eb5bacd0e6c8dd061bdfc68290c
ROllens <zongtang0816@gmail.com>
2018-10-31 17:53:40 +0800

    redstring

--- cityName.py
+++ cityName.py
@@ -1,10 +1,28 @@
 from bs4 import BeautifulSoup
+import os
 
 cityName=[]
+GovData={}
+def OnlyCharNum(s,oth=''):
+	fomart = '0123456789'
+	for c in s:
+	    if not c in fomart:
+	        s = s.replace(c,'')
+	return s
+def CleanN(s,oth=''):
+    for c in s:
+        if c in '\n':
+            s=s.replace(c,'')
+    return s
 
-with open("nds.html",encoding="utf8") as page:
-    soup=BeautifulSoup(page,features="html.parser")
-    for txt in soup.find_all("td",{"headers":"city_Name"}):
-        cityName.append(txt.text)
-
-print(cityName)
+for file in os.listdir("C:\\Users\\User\\Desktop\\BS\\govdata"):
+    if file.endswith(".html"):
+        Path=os.path.join("C:\\Users\\User\\Desktop\\BS\\govdata",file)
+        with open(Path,'rb') as page:
+            soup=BeautifulSoup(page,features="html.parser")
+            for txt in soup.find_all("td",{"headers":"city_Name"}):
+                cityName.append(CleanN(txt.text))
+        print(OnlyCharNum(Path),cityName)
+        #GovData[OnlyCharNum(Path)]=cityName
+        cityName.clear()
+print(GovData)
