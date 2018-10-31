cc23a7a19d5a765cb61664fd7c3ddcbdb38c8849
ROllens <zongtang0816@gmail.com>
2018-10-31 17:54:45 +0800

    redstring

--- cityName.py
+++ cityName.py
@@ -18,11 +18,14 @@ def CleanN(s,oth=''):
 for file in os.listdir("C:\\Users\\User\\Desktop\\BS\\govdata"):
     if file.endswith(".html"):
         Path=os.path.join("C:\\Users\\User\\Desktop\\BS\\govdata",file)
+        print(OnlyCharNum(Path))
         with open(Path,'rb') as page:
             soup=BeautifulSoup(page,features="html.parser")
-            for txt in soup.find_all("td",{"headers":"city_Name"}):
-                cityName.append(CleanN(txt.text))
-        print(OnlyCharNum(Path),cityName)
+            for txt in soup.find_all(color="#FF0000"):
+                flag=txt.parent.previous_sibling
+                if flag:
+                    print(flag.string)
+        #print(OnlyCharNum(Path),cityName)
         #GovData[OnlyCharNum(Path)]=cityName
-        cityName.clear()
-print(GovData)
+        #cityName.clear()
+#print(GovData)
